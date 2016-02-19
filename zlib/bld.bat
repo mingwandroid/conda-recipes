5fc5a4e0f265b76c5d27a29d1de2831932389b24
Ray Donnelly <mingw.android@gmail.com>
2016-02-19 11:46:42 +0000

    zlib: Add cmake + jom build dependencies
    And use cmake --build to do the work.
    Also reformat things a little.

--- zlib/bld.bat
+++ zlib/bld.bat
@@ -1,17 +1,24 @@
+set CMAKE_GENERATOR=NMake Makefiles JOM
+
 mkdir build
 cd build
 
 REM Configure step
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %SRC_DIR%
+cmake -G "%CMAKE_GENERATOR%" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+      %SRC_DIR%
 if errorlevel 1 exit 1
 
 REM Build step
-devenv %PKG_NAME%.sln /Build "%RELEASE_TARGET%"
+cmake --build .
 if errorlevel 1 exit 1
 
 REM Install step
-devenv %PKG_NAME%.sln /Build "%RELEASE_TARGET%" /Project INSTALL
+cmake --build . --target install
 if errorlevel 1 exit 1
 
-REM Some OSS libraries are happier if z.lib exists, even though it's not typical on Windows
+REM Some OSS libraries are happier if z.lib exists
+REM even though it's not typical on Windows
 copy %LIBRARY_LIB%\zlibstatic.lib %LIBRARY_LIB%\z.lib

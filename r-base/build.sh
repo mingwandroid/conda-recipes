8cc6b67e97aab0d2c9fc72d089e3492d49c734c3
Ray Donnelly <mingw.android@gmail.com>
2016-04-01 10:14:59 +0100

    [WIP] r-base: More MSYS2 changes

--- r-base/build.sh
+++ r-base/build.sh
@@ -43,10 +43,13 @@ Linux() {
                 --with-pic                      \
                 --with-cairo                    \
                 LIBnn=lib
+
+    make -j${CPU_COUNT}
+    make install
 }
 
 # This was an attempt to see how far we could get with using Autotools as things
-# stand. On 3.2.2, the build system attempts to compile the Uix code which works
+# stand. On 3.2.4, the build system attempts to compile the Unix code which works
 # to an extent, finally falling over due to fd_set references in sys-std.c when
 # it should be compiling sys-win32.c instead. Eventually it would be nice to fix
 # the Autotools build framework so that can be used for Windows builds too.
@@ -68,7 +71,7 @@ Mingw_w64_autotools() {
     if [[ "${ARCH}" == "64" ]]; then
         export CPPFLAGS="$CPPFLAGS -DWIN=64 -DMULTI=64"
     fi
-    ./configure --prefix=$PREFIX                \
+    ./configure --prefix=${PREFIX}              \
                 --enable-shared                 \
                 --enable-R-shlib                \
                 --enable-BLAS-shlib             \
@@ -80,6 +83,9 @@ Mingw_w64_autotools() {
                 --with-x=no                     \
                 --with-readline=no              \
                 LIBnn=lib
+
+    make -j${CPU_COUNT}
+    make install
 }
 
 # Use the hand-crafted makefiles.
@@ -96,20 +102,96 @@ Mingw_w64_makefiles() {
     echo "BINPREF = "                           >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
     echo "BINPREF64 = "                         >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
     echo "USE_ATLAS = NO"                       >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
-    echo "BUILD_HTML = NO"                      >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
+    echo "BUILD_HTML = YES"                     >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
     echo "WIN = ${ARCH}"                        >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
     echo "EOPTS = -march=${CPU} -mtune=generic" >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
     echo "OPENMP = -fopenmp"                    >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
     echo "PTHREAD = -pthread"                   >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
     echo "COPY_RUNTIME_DLLS = 1"                >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
-    echo "MAKEINFO = makeinfo"                  >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
+    echo "TEXI2ANY = texi2any"                  >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
     echo "TCL_VERSION = 86"                     >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
+    echo "ISDIR = isdir"                        >> "${SRC_DIR}/src/gnuwin32/MkRules.local"
+
+    # The build process copies this across if it finds it and rummaging about on
+    # the website I found a file, so why not, eh?
+    mkdir "${SRC_DIR}/etc"
+    curl -c "${SRC_DIR}/etc/curl-ca-bundle.crt" -SLO https://www.stats.ox.ac.uk/pub/Rtools/goodies/multilib/curl-ca-bundle.crt
+
+    # The hoops we must jump through to get innosetup installed in an unattended way.
+    curl -c innoextract-1.6-windows.zip http://constexpr.org/innoextract/files/innoextract-1.6/innoextract-1.6-windows.zip
+    unzip -o innoextract-1.6-windows.zip
+    curl -c isetup-5.5.8-unicode.exe -SLO http://files.jrsoftware.org/is/5/isetup-5.5.8-unicode.exe
+    ./innoextract.exe ./isetup-5.5.8-unicode.exe
+    mv app isdir
+
+    # RTools33.exe installs an ActiveState TCL in C:/R64. I'll call this ASTCL from now on.
+
+    # tktable, bwidget and tcl/lib need to be copied into Tcl.
+    mkdir -p "${SRC_DIR}/Tcl"
+
+    # In ASTCL, this is located at ${SRC_DIR}/Tcl/lib/BWidget (unversioned)
+    #cp -Rf ${PREFIX}/Library/mingw-w64/lib/bwidget* ${SRC_DIR}/Tcl/lib/
+    # In ASTCL, this is located at ${SRC_DIR}/Tcl/lib64/Tktable (unversioned)
+    #cp -Rf ${PREFIX}/Library/mingw-w64/lib/Tktable* ${SRC_DIR}/Tcl/lib/
+    # In ASTCL, there are also dde1.3 and reg1.2 folders in lib64.
+
+    # We're going for a fairly unusual approach here, I know, but it should be fine
+    # since the DLL dependencies have already been installed and will be on the path.
+    conda install --no-deps --copy --prefix "${SRC_DIR}/Tcl" m2-mingw-w64-tcl m2-mingw-w64-tk m2-mingw-w64-bwidget m2-mingw-w64-tktable
+
+    # Horrible. We need MiKTeX or something like it (for pdflatex.exe. Building from source
+    # may be posslbe but requires CLisp and I've not got time for that at present).  w32tex
+    # looks a little less horrible than MiKTex (just read their build instructions and cry:
+    # For  example:
+    # Cygwin
+    # Hint: install all packages, or be prepared to install missing packages later, when
+    #       CMake fails to find them...
+    # So, let's try with standard w32tex instead: http://w32tex.org/
+    # I wanted to use /tmp/w32tex here but am getting permissions errors trying to execute
+    # texinst2016.exe .. need to investigate how I am mounting my temp dir.
+
+    # W32TeX doesn't have inconsolata.sty which is
+    # needed for R 3.2.4 (later Rs have switched to zi4
+    # instead), I've switched to miktex instead.
+    _use_w32tex=no
+    if [[ "${_use_w32tex}" == "yes" ]]; then
+      mkdir w32tex || true
+        pushd w32tex
+        curl -SLO http://ctan.ijs.si/mirror/w32tex/current/texinst2016.zip
+        unzip -o texinst2016.zip
+        mkdir archives || true
+          pushd archives
+            for _file in latex mftools platex pdftex-w32 ptex-w32 web2c-lib web2c-w32 \
+                         datetime2 dvipdfm-w32 dvipsk-w32 jtex-w32 ltxpkgs luatexja \
+                         luatex-w32 makeindex-w32 manual newtxpx-boondoxfonts pgfcontrib \
+                         t1fonts tex-gyre timesnew ttf2pk-w32 txpx-pazofonts vf-a2bk \
+                         xetex-w32 xindy-w32 xypic; do
+              curl -c ${_file}.tar.xz -SLO http://ctan.ijs.si/mirror/w32tex/current/${_file}.tar.xz
+            done
+          popd
+        ./texinst2016.exe ${PWD}/archives
+        ls -l ./texinst2016.exe
+        mount
+        PATH=${PWD}/bin:${PATH}
+      popd
+    else
+      mkdir miktex || true
+      pushd miktex
+        curl -c miktex-portable-2.9.5857.exe -SLO http://mirrors.ctan.org/systems/win32/miktex/setup/miktex-portable-2.9.5857.exe
+        echo "Extracting miktex-portable-2.9.5857.exe, this will take some time ..."
+        7za x -y miktex-portable-2.9.5857.exe > /dev/null
+        # We also need the url, incolsolata and mptopdf packages and
+        # do not want a GUI to prompt us about installing these.
+        sed -i 's|AutoInstall=2|AutoInstall=1|g' miktex/config/miktex.ini
+        PATH=${PWD}/miktex/bin:${PATH}
+      popd
+    fi
 
     cd "${SRC_DIR}/src/gnuwin32"
-    make distribution
+    make distribution -j${CPU_COUNT}
     cd installer
     make imagedir
-    cp -rf R-3.2.2 "${PREFIX}"/R
+    cp -rf R-3.2.4 "${PREFIX}"/R
 }
 
 Darwin() {
@@ -137,6 +219,9 @@ EOF
                 --enable-R-shlib                    \
                 --without-x                         \
                 --enable-R-framework=no
+
+    make -j${CPU_COUNT}
+    make install
 }
 
 case `uname` in
@@ -151,6 +236,3 @@ case `uname` in
         Mingw_w64_makefiles
         ;;
 esac
-
-make -j4
-make install

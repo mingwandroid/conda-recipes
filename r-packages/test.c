25e07f7c44481aec3873213eac3315be924a095f
Ray Donnelly <mingw.android@gmail.com>
2016-04-12 16:24:10 +0100

    remove a file I didn't mean to commit

--- r-packages/test.c
+++
@@ -1,8 +0,0 @@
-typedef struct _igraph_interruption_handler_t
-{
-	int a;
-} igraph_interruption_handler_t;
-
-extern IGRAPH_THREAD_LOCAL igraph_interruption_handler_t 
-  *igraph_i_interruption_handler;
-
